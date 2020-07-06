548a7d9c933e17235c900e8d04a37664d3c57475
eduardogalvao <eduardotorresgalvao@gmail.com>
2020-07-06 19:04:12 -0300

    Gerando projeto

---
+++ back-end-maxima/src/main/java/br/com/maxima/BackEndMaximaApplication.java
@@ -0,0 +1,13 @@
+package br.com.maxima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndMaximaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndMaximaApplication.class, args);
+	}
+
+}
---
+++ back-end-maxima/src/test/java/br/com/maxima/BackEndMaximaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.maxima;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndMaximaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
